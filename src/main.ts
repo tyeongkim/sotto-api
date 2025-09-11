1f498de0bb06feb1f335127731c55013b2715c2c
Taeyeong Kim <taeyeong.kim@algorix.io>
2025-09-11 18:26:01 +0900

    fix: add tauri localhost to cors origin list

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,11 @@ async function bootstrap() {
 
 	const corsOptions = isProduction
 		? {
-				origin: ['https://sotto.tyeongk.im', 'https://sotto-viewer.tyeongk.im'],
+				origin: [
+					'https://sotto.tyeongk.im',
+					'https://sotto-viewer.tyeongk.im',
+					'tauri://localhost',
+				],
 				credentials: true,
 				methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 				allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
