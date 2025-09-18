1f52e084df3ea906fb4ae363590602f3d55267c9
Taeyeong Kim <taeyeong.kim@algorix.io>
2025-09-18 19:35:15 +0900

    fix: update tauri localhost to CORS allowed origins

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
 					'https://sotto.tyeongk.im',
 					'https://sotto-viewer.tyeongk.im',
 					'tauri://localhost',
+					'http://tauri.localhost',
 				],
 				credentials: true,
 				methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
